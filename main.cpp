2150433862b804328761cc14e72a638c21beb70d
Dytia <69058656+Dytia@users.noreply.github.com>
2021-09-01 13:27:59 +0000

    got it working, do not use repl for the code changes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
+#include <Servo.h>
 using namespace std;
+
+Servo myservo;
+
+int pos = 0;
+
+void setup() {
+  myservo.attach(9);
+}
+
+void function(){
+  cout<<"mew\n";
+}
 
 int main() 
 {
   cout << "Hello World!\n";
-  void myFunction() {
-    cout << "hihi\n";
-  }
-  myFunction();
+  function();
   return 0;
   
+  for (pos = 0; pos <= 180; pos += 1) {
+    // in steps of 1 degree
+    myservo.write(pos);
+    delay(15);
+  }
+  for (pos = 180; pos >= 0; pos -= 1) {
+    myservo.write(pos);
+    delay(15);
+  }
 }
